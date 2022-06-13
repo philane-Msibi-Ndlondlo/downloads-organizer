daf9caeb772ba8d3070ea4e00b9e776dfc3a58c6
Philane-Msibi-Ndlondlo <philanemsibi14@gmail.com>
2022-06-13 22:42:39 +0200

    Added Video, Music and Documents Support

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ allowed_music_extensions = [".mp3", ".wav"]
 allowed_document_extensions = [".mp3", ".wav"]
 
 failed_to_move_files = []
-moved_images = []
+moved_files = []
 
 if os.path.isdir(downloads_folder):
 
@@ -24,7 +24,6 @@ if os.path.isdir(downloads_folder):
         for file in downloads_folder_files:
 
             downloads_file_full_name = os.path.join(downloads_folder, file)
-            pictures_file_full_name = os.path.join(pictures_folder, file)
 
             if os.path.isfile(downloads_file_full_name):
 
@@ -32,19 +31,84 @@ if os.path.isdir(downloads_folder):
 
                 if extension in allowed_image_extensions:
                     
+                    pictures_file_full_name = os.path.join(pictures_folder, file)
+
                     if os.path.exists(pictures_file_full_name):
                         failed_to_move_files.append(file)
                         continue
 
                     try:
-                        #os.rename(downloads_file_full_name, pictures_file_full_name)
-                        moved_images.append(file)
+                        os.rename(downloads_file_full_name, pictures_file_full_name)
+                        moved_files.append(f'{file} moved to {pictures_folder}')
                     except:
+                        failed_to_move_files.append(f'{file} failed to move to {pictures_folder}')
+                        continue
+
+                elif extension in allowed_video_extensions:
+                    
+                    videos_file_full_name = os.path.join(videos_folder, file)
+
+                    if os.path.exists(videos_file_full_name):
                         failed_to_move_files.append(file)
                         continue
 
+                    try:
+                        os.rename(downloads_file_full_name, videos_file_full_name)
+                        moved_files.append(f'{file} moved to {videos_folder}')
+                    except:
+                        failed_to_move_files.append(f'{file} failed to move to {videos_folder}')
+                        continue
 
+                elif extension in allowed_music_extensions:
+                    
+                    music_file_full_name = os.path.join(music_folder, file)
 
-    print(str(len(moved_images)) + " files moved")
+                    if os.path.exists(music_file_full_name):
+                        failed_to_move_files.append(file)
+                        continue
+
+                    try:
+                        os.rename(downloads_file_full_name, music_file_full_name)
+                        moved_files.append(f'{file} moved to {music_folder}')
+                    except:
+                        failed_to_move_files.append(f'{file} failed to move to {music_folder}')
+                        continue
+
+                elif extension in allowed_document_extensions:
+                    
+                    document_file_full_name = os.path.join(documents_folder, file)
+
+                    if os.path.exists(document_file_full_name):
+                        failed_to_move_files.append(file)
+                        continue
+
+                    try:
+                        os.rename(downloads_file_full_name, document_file_full_name)
+                        moved_files.append(f'{file} moved to {documents_folder}')
+                    except:
+                        failed_to_move_files.append(f'{file} failed to move to {documents_folder}')
+                        continue
+                else:
+                    failed_to_move_files.append('{file} failed to move: Invalid File Extension')
+
+    print('************************')
+    print('** MOVED FILES **')
+    print('************************')
+    
+    for moved_file in moved_files:
+        print(moved_file)
+
+    print("\n" + str(len(moved_files)) + " files moved!! ")
+
+    print("\n")
+    print('************************')
+    print('** FAILED MOVE FILES **')
+    print('************************')
+    print("\n")
+    print(str(len(failed_to_move_files)) + " files failed to be moved!! ")
+
+    for failed_move_file in failed_to_move_files:
+        print(failed_move_file)
+
 else:
     print("Invalid Downloads Folder")
